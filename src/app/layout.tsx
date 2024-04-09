c20acf16f4e6eeef7045682a784c2b88e0ed649f
Tomas Pilnaj <pilnaj.t@gmail.com>
2024-04-09 10:31:30 +0200

    npm #1: setup MUI theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
 import "./globals.css";
 
+import { ThemeProvider } from "@mui/material/styles";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
+import theme from "../theme";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +21,11 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				<AppRouterCacheProvider>
+					<ThemeProvider theme={theme}>{children}</ThemeProvider>
+				</AppRouterCacheProvider>
+			</body>
 		</html>
 	);
 }
