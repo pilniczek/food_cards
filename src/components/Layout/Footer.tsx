ab9fcc7e150785d311290848bea4ecd59befbb51
Tomas Pilnaj <pilnaj.t@gmail.com>
2024-04-10 15:22:41 +0200

    feat #3: created Footer

---
+++ src/components/Layout/Footer.tsx
@@ -0,0 +1,11 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+const Footer = () => (
+	<Box p={2}>
+		<Typography variant="body1" component="p" textAlign="center">
+			🍽️2024
+		</Typography>
+	</Box>
+);
+export default Footer;
