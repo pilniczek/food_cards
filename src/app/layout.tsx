8be6fcd9ccfb61acce547b40736e1c7007f36bdb
Tomas Pilnaj <pilnaj.t@gmail.com>
2024-04-11 10:48:31 +0200

    fix #5: setup layouts properly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,41 +1,17 @@
-import CssBaseline from "@mui/material/CssBaseline";
-import { ThemeProvider } from "@mui/material/styles";
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { type ReactNode } from "react";
 
-import Footer from "@/components/Layout/Footer";
-
-import theme from "../theme";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export const metadata: Metadata = {
-	title: "404",
-	description: "Stránka nenalezena.",
+	title: "Food Cards",
+	description: "Co bude dnes k večeři?",
 };
 
 const RootLayout = ({
 	children,
-	params: { locale },
 }: Readonly<{
 	children: ReactNode;
-	params: { locale: "cs" };
 }>): ReactNode => {
-	return (
-		<html lang={locale}>
-			<body className={inter.className}>
-				<AppRouterCacheProvider>
-					<ThemeProvider theme={theme}>
-						<CssBaseline />
-						{children}
-						<Footer />
-					</ThemeProvider>
-				</AppRouterCacheProvider>
-			</body>
-		</html>
-	);
+	return children;
 };
 
 export default RootLayout;
