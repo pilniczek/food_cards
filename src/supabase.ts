18db61dc52380f9b0e771077e42512424c110494
Tomas Pilnaj <pilnaj.t@gmail.com>
2024-04-13 21:48:08 +0200

    feat #9: create supabase client

---
+++ src/supabase.ts
@@ -0,0 +1,12 @@
+// https://supabase.com/docs/reference/javascript/initializing
+
+import { createClient } from "@supabase/supabase-js";
+
+import { Database } from "@/types/supabase";
+
+const supabase = createClient<Database>(
+	process.env.NEXT_PUBLIC_SUPABASE_URL,
+	process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+);
+
+export default supabase;
