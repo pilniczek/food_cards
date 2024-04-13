436130d107e7aea5d3cdca7ed8c8dbafad8509f6
Tomas Pilnaj <pilnaj.t@gmail.com>
2024-04-13 17:51:50 +0200

    feat #9: move username input to settings

--- src/app/[locale]/settings/page.tsx
+++ src/app/[locale]/settings/page.tsx
@@ -1,8 +1,10 @@
+import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { useTranslations } from "next-intl";
 import { type ReactNode } from "react";
 
 import BackButton from "@/components/Button/Back";
+import FormSetUsername from "@/components/Form/SetUsername";
 import Header from "@/components/Layout/Header";
 
 const Settings = (): ReactNode => {
@@ -16,6 +18,12 @@ const Settings = (): ReactNode => {
 				<Typography variant="h4" component="h1" textAlign="center">
 					{translate("Settings.title")}
 				</Typography>
+				<Box>
+					<Typography component="h2" variant="h5">
+						{translate("Index.description")}
+					</Typography>
+					<FormSetUsername translate={{ submit: translate("Global.submit") }} />
+				</Box>
 			</main>
 		</>
 	);
