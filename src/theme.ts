6d84a2f75d284946275aade1b5531b78cef129dc
Tomas Pilnaj <pilnaj.t@gmail.com>
2024-04-13 18:39:10 +0200

    feat #9: utilize mui theme

--- src/theme.ts
+++ src/theme.ts
@@ -26,4 +26,29 @@ theme!.components!.MuiCardActionArea = {
 	},
 };
 
+theme!.components!.MuiAppBar = {
+	styleOverrides: {
+		root: { marginBottom: theme.spacing(3) },
+	},
+};
+
+declare module "@mui/material/Button" {
+	interface ButtonPropsVariantOverrides {
+		login: true;
+	}
+}
+
+theme!.components!.MuiButton = {
+	variants: [
+		{
+			props: { variant: "login" },
+			style: {
+				textAlign: "left",
+				textTransform: "unset",
+				border: `1px solid ${theme.palette.grey[300]}`,
+			},
+		},
+	],
+};
+
 export default theme;
