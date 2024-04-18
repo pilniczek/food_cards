660638aa687e652a0e8e1baae3a8a08b95d5c444
Tomas Pilnaj <pilnaj.t@gmail.com>
2024-04-18 10:21:00 +0200

    refactor #15: login as button onClick instead of submit form

--- src/components/Form/LoginGithub/index.tsx
+++ src/components/Form/LoginGithub/index.tsx
@@ -1,41 +1,27 @@
 "use client";
 import Box from "@mui/material/Box";
-import Stack from "@mui/material/Stack";
 import Image from "next/image";
 import { useLocale } from "next-intl";
-import { type FormEvent, type ReactNode } from "react";
-import { FormProvider, useForm } from "react-hook-form";
+import { type ReactNode } from "react";
 
 import SubmitButton from "@/components/Form/SubmitButton";
 import getURL from "@/utils/getUrl";
 import signIn from "@/utils/signIn";
 
 const LoginGithub = ({ translate }: { translate: { submit: string } }): ReactNode => {
-	const methods = useForm();
 	const locale = useLocale();
 
-	const handleFormSubmit = (e: FormEvent<HTMLFormElement>): void => {
-		e.preventDefault();
-		methods.handleSubmit(() => signIn(`${getURL()}${locale}/dashboard`, "github"))();
-	};
-
 	return (
-		<FormProvider {...methods}>
-			<form onSubmit={handleFormSubmit}>
-				<Stack spacing={4} alignItems="center">
-					<SubmitButton variant="login">
-						<Box sx={{ pr: 2 }}>{translate.submit}</Box>
-						<Image
-							src="/images/github-logo.webp"
-							alt={translate.submit}
-							width="60"
-							height="60"
-							style={{ objectFit: "contain" }}
-						/>
-					</SubmitButton>
-				</Stack>
-			</form>
-		</FormProvider>
+		<SubmitButton variant="login" onClick={() => signIn(`${getURL()}${locale}/dashboard`, "github")}>
+			<Box sx={{ pr: 2 }}>{translate.submit}</Box>
+			<Image
+				src="/images/github-logo.webp"
+				alt={translate.submit}
+				width="60"
+				height="60"
+				style={{ objectFit: "contain" }}
+			/>
+		</SubmitButton>
 	);
 };
 
