6b56c70384206e2da06bdd46744f6c0a2e5d7776
Tomas Pilnaj <pilnaj.t@gmail.com>
2024-04-21 00:50:46 +0200

    feat #17: authorization flow moved to react query

--- src/components/Form/LoginGithub/index.tsx
+++ src/components/Form/LoginGithub/index.tsx
@@ -5,14 +5,20 @@ import { useLocale } from "next-intl";
 import { type ReactNode } from "react";
 
 import SubmitButton from "@/components/Form/SubmitButton";
+import useSignIn from "@/query-hooks/useSignIn";
 import getURL from "@/utils/getUrl";
-import signIn from "@/utils/signIn";
 
 const LoginGithub = ({ translate }: { translate: { submit: string } }): ReactNode => {
 	const locale = useLocale();
+	const { mutate } = useSignIn();
 
+	const onSignIn = () => {
+		mutate(
+			{ url: `${getURL()}${locale}/dashboard`, provider: "github" }, // TODO error
+		);
+	};
 	return (
-		<SubmitButton variant="login" onClick={() => signIn(`${getURL()}${locale}/dashboard`, "github")}>
+		<SubmitButton variant="login" onClick={onSignIn}>
 			<Box sx={{ pr: 2 }}>{translate.submit}</Box>
 			<Image
 				src="/images/github-logo.webp"
--- src/components/Form/LoginGoogle/index.tsx
+++ src/components/Form/LoginGoogle/index.tsx
@@ -5,14 +5,20 @@ import { useLocale } from "next-intl";
 import { type ReactNode } from "react";
 
 import SubmitButton from "@/components/Form/SubmitButton";
+import useSignIn from "@/query-hooks/useSignIn";
 import getURL from "@/utils/getUrl";
-import signIn from "@/utils/signIn";
 
 const LoginGoogle = ({ translate }: { translate: { submit: string } }): ReactNode => {
 	const locale = useLocale();
+	const { mutate } = useSignIn();
 
+	const onSignIn = () => {
+		mutate(
+			{ url: `${getURL()}${locale}/dashboard`, provider: "google" }, // TODO error
+		);
+	};
 	return (
-		<SubmitButton variant="login" onClick={() => signIn(`${getURL()}${locale}/dashboard`, "google")}>
+		<SubmitButton variant="login" onClick={onSignIn}>
 			<Box sx={{ pr: 2 }}>{translate.submit}</Box>
 			<Image src="/images/google-logo.svg" alt={translate.submit} width="60" height="60" />
 		</SubmitButton>
