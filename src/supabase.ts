72f451d7c231988481b9d7ea566236c4a1df202f
Tomas Pilnaj <pilnaj.t@gmail.com>
2024-04-21 00:46:45 +0200

    feat #17: add supabase types

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,10 +1,12 @@
 // https://supabase.com/docs/reference/javascript/initializing
 
-import { createClient } from "@supabase/supabase-js";
+import { createClient, type SupabaseClient } from "@supabase/supabase-js";
 
 import { Database } from "@/types/supabase";
 
 export { type Provider } from "@supabase/supabase-js";
+
+export type TypedSupabaseClient = SupabaseClient<Database>;
 
 const supabase = createClient<Database>(
 	process.env.NEXT_PUBLIC_SUPABASE_URL,
