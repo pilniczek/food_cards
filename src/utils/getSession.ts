65ea2c8719955ac6665f9be9cf1949bd37aced58
Tomas Pilnaj <pilnaj.t@gmail.com>
2024-04-21 03:05:29 +0200

    refactor #17: remove unused files

--- src/utils/getSession.ts
+++
@@ -1,8 +0,0 @@
-import supabase from "@/supabase";
-
-const getSession = async () => {
-	const response = await supabase.auth.getSession();
-	return response.data.session;
-};
-
-export default getSession;
